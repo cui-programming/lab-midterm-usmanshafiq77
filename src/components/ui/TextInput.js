ee57f83ea8e327167ab60ea461e6ffa56c21624a
usmanshafiq77 <usmanshafiq342@gmail.com>
2025-10-20 01:01:38 +0500

    Update TextInput.js

--- src/components/ui/TextInput.js
+++ src/components/ui/TextInput.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TextInput as RNTextInput } from 'react-native';
+import styles from '../../styles/styles';
 
 export default function TextInput(props) {
-  return <RNTextInput {...props} />;
+  return <RNTextInput style={[styles.textInput, props.style]} {...props} />;
 }
