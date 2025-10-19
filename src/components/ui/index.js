96c0d0f942fb524b0ca8eb00eea488775aa813aa
usmanshafiq77 <usmanshafiq342@gmail.com>
2025-10-20 01:01:57 +0500

    Update index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,6 +1,5 @@
-// Barrel exports for UI components.
-// Intentionally missing the export for './Text' (students must add it).
 export { default as Button } from './Button';
+export { default as Text } from './Text';
+export { default as View } from './View';
+export { default as ScrollView } from './ScrollView';
 export { default as TextInput } from './TextInput';
-export { default as Text } from '../ui'; 
-// export { default as Text } from './Text'; // <-- students will uncomment/add this
