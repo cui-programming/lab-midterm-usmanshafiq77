687ba04b8556f2ef5ea2f98c7179a6575c2cb063
usmanshafiq77 <usmanshafiq342@gmail.com>
2025-10-19 10:04:54 +0500

    Update student.js

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,4 @@
 // Edit these two fields with your details before you start.
 // They will be shown at the very top of the screen.
-export const STUDENT_NAME = "Your Name";
-export const REG_NO = "REG-000000";
+export const STUDENT_NAME = "Usman Shafiq";
+export const REG_NO = "SP"23-BSE-012";
